2f23f93311b702038c082503e5a116ecf3e58d1c
Tim McEwan <blk_hotsauce@yahoo.com>
2017-11-28 14:04:23 -0700

    Add segue and usage controls to send URL over to the next view controller

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by sudo on 11/28/17.
+//  Copyright © 2017 sudo. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    var recordAudioURL: URL!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(String(describing: recordAudioURL))
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecordSoundsViewController: UIViewController {
+
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    var audioRecorder: AVAudioRecorder!
+
     @IBOutlet weak var textOUT: UILabel!
     @IBOutlet weak var recordOUT: UIButton!
     @IBOutlet weak var stopRecordingOUT: UIButton!
@@ -16,26 +21,60 @@ class RecordSoundsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello this is started")
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     @IBAction func recordNow(_ sender: Any) {
         textOUT.text = "Recording in Progress"
         stopRecordingOUT.isEnabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
     }
     
     @IBAction func stopACN(_ sender: Any) {
         textOUT.text = "Press to record"
         stopRecordingOUT.isEnabled = false
-
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     
     }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+        if flag {
+             performSegue(withIdentifier: "toSecond", sender: audioRecorder.url)
+        }else {
+            print("Recording failed")
+        }
+       
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecond" {
+        let playSoundsVC = segue.destination as! PlaySoundsViewController
+        let recordedAudioURL = sender as! URL
+            playSoundsVC.recordAudioURL = recordedAudioURL
+            
+        }
+    }
+    
+    
 }
 
